9dcdd53d77baa58eb596d7d3fcff8536ac3176a3
aman2208 <amankumar.kumar369@gamail.com>
2023-08-12 10:54:59 +0000

    initial code

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import {Component} from 'react'
+import CartContext from './Context/CartContext'
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+class App extends Component {
+  state = {cartList: []}
+
+  addingmyitm = items => {
+    this.setState(prev => ({cartList: [...prev.cartList, items]}))
+  }
+
+  render() {
+    const {cartList} = this.state
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addtoitem: this.addingmyitm,
+          deletetoitem: this.deletetoitem,
+        }}
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/login" component={LoginForm} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute exact path="/products" component={Products} />
+            <ProtectedRoute
+              exact
+              path="/products/:id"
+              component={ProductItemDetails}
+            />
+            <ProtectedRoute exact path="/cart" component={Cart} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="not-found" />
+          </Switch>
+        </BrowserRouter>
+      </CartContext.Provider>
+    )
+  }
+}
+
+export default App
